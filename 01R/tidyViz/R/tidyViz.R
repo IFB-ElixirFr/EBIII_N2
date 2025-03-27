af1f4abddbbb514ff6f69df351908009bd878023
lkhamvongsa <lucie.khamvongsa@gmail.com>
2025-03-27 13:36:21 +0100

    Ajout des cours R 2021

---
+++ 01R/tidyViz/R/tidyViz.R
@@ -0,0 +1,4 @@
+#' Package pour la formation EBAII niveau 2
+#' Contient les vignettes (les slides de cours), le jeu de données "fruits" et les tutoriels. https://ifb-elixirfr.github.io/EBAII/2021/ebaiin2/ 
+#' @keywords internal
+"_PACKAGE"
