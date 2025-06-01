b204b3b5659cad16a6e6d412d3c7d91f835df454
Cathy Philippe <cathy.philippe@cea.fr>
2025-06-01 16:44:04 +0200

    Add output repository

--- DNAseq/GWAS_tutorial/scripts/MAF_check.R
+++ DNAseq/GWAS_tutorial/scripts/MAF_check.R
@@ -1,5 +1,5 @@
-maf_freq <- read.table("MAF_check.frq", header =TRUE, as.is=T)
-pdf("MAF_distribution.pdf")
+maf_freq <- read.table("results/MAF_check.frq", header =TRUE, as.is=T)
+pdf("results/MAF_distribution.pdf")
 hist(maf_freq[,5],main = "MAF distribution", xlab = "MAF")
 dev.off()
 
--- DNAseq/GWAS_tutorial/scripts/hist_miss.R
+++ DNAseq/GWAS_tutorial/scripts/hist_miss.R
@@ -1,10 +1,10 @@
-indmiss<-read.table(file="plink.imiss", header=TRUE)
-snpmiss<-read.table(file="plink.lmiss", header=TRUE)
+indmiss<-read.table(file="data/plink.imiss", header=TRUE)
+snpmiss<-read.table(file="data/plink.lmiss", header=TRUE)
 # read data into R 
 
-pdf("histimiss.pdf") #indicates pdf format and gives title to file
+pdf("results/histimiss.pdf") #indicates pdf format and gives title to file
 hist(indmiss[,6],main="Histogram individual missingness") #selects column 6, names header of file
 
-pdf("histlmiss.pdf") 
+pdf("results/histlmiss.pdf") 
 hist(snpmiss[,5],main="Histogram SNP missingness")  
 dev.off() # shuts down the current device
