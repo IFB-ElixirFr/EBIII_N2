9690794a8d1dd54e73beaf100d337907923921f3
Cathy Philippe <cathy.philippe@cea.fr>
2025-06-01 16:44:04 +0200

    Add output repository

--- DNAseq/GWAS_tutorial/scripts/MDS_merged.R
+++ DNAseq/GWAS_tutorial/scripts/MDS_merged.R
@@ -1,9 +1,9 @@
-data<- read.table(file="MDS_merge2.mds",header=TRUE)
-race<- read.table(file="racefile.txt",header=TRUE)
-datafile<- merge(data,race,by=c("IID","FID"))
+data<- read.table(file="data/MDS_merge2.mds",header=TRUE)
+ancestry<- read.table(file="data/ancestry_file.txt",header=TRUE)
+datafile<- merge(data,ancestry,by=c("IID","FID"))
 head(datafile)
 
-pdf("MDS.pdf",width=7,height=7)
+pdf("results/MDS.pdf",width=7,height=7)
 for (i in 1:nrow(datafile))
 {
 if (datafile[i,14]=="EUR") {plot(datafile[i,4],datafile[i,5],type="p",xlim=c(-0.1,0.2),ylim=c(-0.15,0.1),xlab="MDS Component 1",ylab="MDS Component 2",pch=1,cex=0.5,col="green")}
--- DNAseq/GWAS_tutorial/scripts/hwe.R
+++ DNAseq/GWAS_tutorial/scripts/hwe.R
@@ -1,9 +1,9 @@
-hwe<-read.table (file="plink.hwe", header=TRUE)
-pdf("histhwe.pdf")
+hwe<-read.table (file="results/plink.hwe", header=TRUE)
+pdf("results/histhwe.pdf")
 hist(hwe[,9],main="Histogram HWE")
 dev.off()
 
-hwe_zoom<-read.table (file="plinkzoomhwe.hwe", header=TRUE)
-pdf("histhwe_below_theshold.pdf")
+hwe_zoom<-read.table (file="results/plinkzoomhwe.hwe", header=TRUE)
+pdf("results/histhwe_below_theshold.pdf")
 hist(hwe_zoom[,9],main="Histogram HWE: strongly deviating SNPs only")
 dev.off()
